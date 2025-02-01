4a151531e36e3748cab1aae34d667b07fcba5dca
seifRashid <rseif1218@gmail.com>
2025-02-01 17:03:39 +0300

    Complited the assignment

--- Bank.js
+++ Bank.js
@@ -7,7 +7,15 @@ class Bank {
 
     // Add methods here:
     // Example: createAccount(name, initialDeposit)
-
+    createAccount(name, initialDeposit) {
+        const account = {
+            name: name,
+            balance: initialDeposit
+        };
+        this.accounts.push(account);
+        const newUserAccount = new Account(account.name, account.balance)
+        return newUserAccount;
+    }
 }
 
 // Account Class: Represents a single user's account
@@ -21,15 +29,41 @@ class Account {
     // Add methods here:
     // Example: deposit(amount) 
     // example data to be stored in transactionHistory { transactionType: 'Deposit', amount: 500 }
+    deposit(amount) {
+        this.balance += amount;
+        this.transactionHistory.push({ transactionType: 'Deposit', amount: amount });
+    }
 
     // Example: withdraw(amount)
     // example data to be stored in transactionHistory { transactionType: 'Withdrawal', amount: 200 }
+    withdraw(amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            this.transactionHistory.push({ transactionType: 'Withdrawal', amount: amount });
+        } else {
+            console.log('Insufficient funds');
+        }
+    }
 
     // Example: transfer(amount, recipientAccount)
+    transfer(amount, recipientAccount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            recipientAccount.deposit(amount);
+            this.transactionHistory.push({
+                transactionType: 'Transfer', amount: amount, to: recipientAccount.name
+            });
+            recipientAccount.transactionHistory.push({
+                transactionType: 'Deposit', amount: amount, from: this.name
+            });
+        } else {
+            console.log('Insufficient funds');
+        }
+    }
     // example data to be stored in transactionHistory:
     // for account sending { transactionType: 'Transfer', amount: 300, to: recipientName }
     // for account recieving { transactionType: 'Received', amount: 300, from: senderName }
-    
+
     // Example: checkBalance()
 }
 
@@ -58,11 +92,11 @@ function testBankOperations() {
     console.log('Jane\'s balance:', janeFinalBalance);
 
     // Return balances for testing
-    return { 
-        johnFinalBalance, 
-        janeFinalBalance, 
-        johnTransactionHistory: johnAccount.transactionHistory, 
-        janeTransactionHistory: janeAccount.transactionHistory 
+    return {
+        johnFinalBalance,
+        janeFinalBalance,
+        johnTransactionHistory: johnAccount.transactionHistory,
+        janeTransactionHistory: janeAccount.transactionHistory
     };
 }
 
